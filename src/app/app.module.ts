1b07535cd83dedd5a09c53fd5527eb65e6b084a9
Reinier Gonzalez <reyhm09@gmail.com>
2019-09-16 11:37:09 -0400

    Created components and set styles in index.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { DetallesComponent } from './ingreso-egreso/detalles/detalles.component';
+import { EstadisticasComponent } from './ingreso-egreso/estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    IngresoEgresoComponent,
+    DetallesComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule,

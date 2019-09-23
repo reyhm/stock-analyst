44a7f2b213bdbc5b81f04552fe8fbc3e5ffa3f25
Reinier Gonzalez <reyhm09@gmail.com>
2019-09-23 08:34:21 -0400

    Commented id in ingreso-egreso model.

--- src/app/models/ingreso-egreso.model.ts
+++ src/app/models/ingreso-egreso.model.ts
@@ -6,7 +6,7 @@ export class IngresoEgreso {
   amount: number;
 
   constructor(user: any) {
-    this.id          = user && user.id          || null;
+    // this.id          = user && user.id          || null;
     this.type        = user && user.type        || null;
     this.description = user && user.description || null;
     this.amount      = user && user.amount      || null;

f52326408bfafc202c0b1fa26ce0bfe71921eaf5
Reinier Gonzalez <reyhm09@gmail.com>
2019-09-23 09:40:01 -0400

    Created ingreso-egreso reducer and actions.

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,5 +1,6 @@
 import * as fromUI from './ngrx/reducers/ui.reducer';
 import * as fromAuth from './ngrx/reducers/auth.reducer';
+import * as fromIE from './ngrx/reducers/ingreso-egreso.reducer';
 
 import { ActionReducerMap } from '@ngrx/store';
 
@@ -7,9 +8,11 @@ import { ActionReducerMap } from '@ngrx/store';
 export interface AppStage {
   ui: fromUI.Stage;
   auth: fromAuth.AuthState;
+  ingresoEgreso: fromIE.IngresoEgresoState;
 }
 
 export const appReducers: ActionReducerMap<AppStage> = {
   ui: fromUI.UIReducer,
-  auth: fromAuth.authReducer
+  auth: fromAuth.authReducer,
+  ingresoEgreso: fromIE.IngresoEgresoReducer
 };
---
+++ src/app/ngrx/reducers/ingreso-egreso.reducer.ts
@@ -0,0 +1,35 @@
+/* tslint:disable:no-trailing-whitespace */
+import * as fromIE from '../actions/ingreso-egreso.action';
+import { IngresoEgreso } from '../../models/ingreso-egreso.model';
+
+
+export interface IngresoEgresoState {
+  items: IngresoEgreso[];
+}
+
+const initState: IngresoEgresoState = {
+  items: []
+};
+
+export function IngresoEgresoReducer(state = initState, action: fromIE.actions) {
+
+  switch (action.type) {
+
+    case fromIE.SET_ITEMS:
+      return {
+        items: [
+          ...action.items.map((item) => {
+            return {...item};
+          })
+        ]
+      };
+
+    case fromIE.UNSET_ITEMS:
+      return [];
+
+    default:
+      return state;
+
+  }
+
+}

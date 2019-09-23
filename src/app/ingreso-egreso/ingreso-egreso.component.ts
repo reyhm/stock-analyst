7931eb4c6a9743d840e29b79a772a0f112c0bb3a
Reinier Gonzalez <reyhm09@gmail.com>
2019-09-23 08:33:50 -0400

    Implemented loading in ingreso-egreso with ngrx.

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,29 +1,37 @@
 /* tslint:disable:no-trailing-whitespace */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IngresoEgreso } from '../models/ingreso-egreso.model';
 import { IngresoEgresoService } from '../services/ingreso-egreso.service';
 
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { LoadedUIAction, LoadingUIAction } from '../ngrx/actions/ui.action';
+import { AppStage } from '../app.reducer';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
   styles: []
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
   form: FormGroup;
   type: string;
   loading = false;
+  subscribe: Subscription;
 
   /**
    * Contructor
    *
    * @param ieService
+   * @param store
    */
   constructor(
-    private ieService: IngresoEgresoService
+    private ieService: IngresoEgresoService,
+    private store: Store<AppStage>
   ) {
+    this.subscribe = new Subscription();
     this.type = 'ingreso';
   }
 
@@ -35,24 +43,34 @@ export class IngresoEgresoComponent implements OnInit {
       description: new FormControl('', Validators.required),
       amount: new FormControl('', Validators.required)
     });
+
+    this.subscribe = this.store.select('ui')
+      .subscribe((uiStage) => this.loading = uiStage.isLoading);
   }
 
   /**
    * Ingreso egreso
    */
   addIngresoEgreso() {
-    this.loading = true;
+    this.store.dispatch( new LoadingUIAction() );
     const data = new IngresoEgreso({...this.form.value, type: this.type});
 
     this.ieService.addIngresoEgreso( data )
       .then(() => {
-        this.loading = false;
+        this.store.dispatch( new LoadedUIAction() );
         Swal.fire('Created', `the ${this.type} was creted successful`, 'success');
       })
       .catch(err => {
-        this.loading = false;
+        this.store.dispatch( new LoadedUIAction() );
         console.log('Error: ', err);
       });
+  }
+
+  /**
+   * OnDestroy
+   */
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
   }
 
 }
